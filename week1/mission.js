65de153dbd204b46bddbfe6f359cab0609bad36b
LolaCuadra <lola_cuadra1940@byui.edu>
2025-01-23 23:22:02 -0700

    media query

--- week1/mission.js
+++ week1/mission.js
@@ -8,15 +8,22 @@ const themeSelector = document.querySelector(".theme");
 const logo = document.querySelector("footer img");
 
 function changeTheme() {
-    const selectedTheme = themeSelector.value;
-
-    if (selectedTheme === "dark") {
-        document.body.classList.add("dark");
-        logo.src = "./byui-logo_white.png";
-    } else {
-        document.body.classList.remove("dark");
-        logo.src = "./byui-logo_blue.webp";
-    }
+// check to see what the current value of our select is.
+// The current value is conveniently found in themeSelector.value!
+const selectedTheme = themeSelector.value;
+// if the value is dark then:
+// add the dark class to the body
+if (selectedTheme == "dark"){
+    document.body.classList.add("dark");
+    logo.src = "./byui-logo_white.png";
+    // otherwise
+} else {
+    // remove the dark class
+    document.body.classList.remove("dark");
+    // make sure the logo src is the blue logo.
+    logo.src = "./byui-logo_blue.webp"; 
 }
+// add an event listener to the themeSelector element here.
 
-themeSelector.addEventListener("change", changeTheme);
+// Use the changeTheme function as the event handler function.
+themeSelector.addEventListener('change', changeTheme);
